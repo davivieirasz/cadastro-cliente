c546dd96220c111ce038e0e3144e95d747a9c7c3
Davi Vieira <davivieiralm02@hotmail.com>
2026-02-22 12:43:00 -0300

    Melhora fluxo de inicialização do .dpr

--- project/clientes.dpr
+++ project/clientes.dpr
@@ -2,20 +2,35 @@ program clientes;
 
 uses
   Vcl.Forms,
+  System.UITypes,
   Unit_Principal in '..\forms\Unit_Principal.pas' {frmPrincipal},
-  Unit_Clientes in '..\forms\Unit_Clientes.pas' {frm_cadastro_cliente},
+  Unit_Clientes in '..\forms\Unit_Clientes.pas' {frmCadastroCliente},
   Unit_DM in '..\datamodule\Unit_DM.pas' {DM: TDataModule},
-  Unit_Consulta_Cliente in '..\forms\Unit_Consulta_Cliente.pas' {frm_consulta_cliente},
+  Unit_Consulta_Cliente in '..\forms\Unit_Consulta_Cliente.pas' {frmConsultaCliente},
   Unit_Login in '..\forms\Unit_Login.pas' {frmLogin},
-  Unit_Usuarios in '..\forms\Unit_Usuarios.pas' {frmCadastroUsuario};
+  Unit_Usuarios in '..\forms\Unit_Usuarios.pas' {frmCadastroUsuario},
+  Unit_Consulta_Usuario in '..\forms\Unit_Consulta_Usuario.pas' {frmConsultaUsuario};
 
 {$R *.res}
 
+var
+  login: TfrmLogin;
+
+
 begin
   Application.Initialize;
-  Application.MainFormOnTaskbar := True;
-  Application.CreateForm(TfrmLogin, frmLogin);
+  Application.MainFormOnTaskbar := False;
+
   Application.CreateForm(TDM, DM);
-  Application.CreateForm(TfrmCadastroUsuario, frmCadastroUsuario);
+  Application.CreateForm(TfrmPrincipal, frmPrincipal);
+  Application.CreateForm(TfrmConsultaUsuario, frmConsultaUsuario);
+
+  Login := TfrmLogin.Create(nil);
+  try
+    if Login.ShowModal <> mrOk then
+      Halt;
+  finally
+    Login.Free;
+  end;
   Application.Run;
 end.
